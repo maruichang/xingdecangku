5e08f7d21667d5f0634822fb1d2b37d8054eeffa
maruichang <378025342@qq.com>
2019-04-25 15:11:24 +0800

    新创建的

---
+++ src/itdome/tect/TextDome1.java
@@ -0,0 +1,9 @@
+package itdome.tect;
+
+public class TextDome1 {
+    public static void main(String[] args) {
+        System.out.println("相互转发");
+        System.out.println("相互转发");
+        System.out.println("相互转发");
+    }
+}
